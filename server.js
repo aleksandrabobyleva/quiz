fa91be39d27d06aac0f5fe29ad17c3654c0e9806
aleksandrabobyleva <aleksandrabobyleva@gmail.com>
2025-07-27 07:09:01 +0200

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
 
 let teamsData = [
   { name: "Team 1", case1: 0, case2: 0, cipher: 0 },
@@ -25,6 +27,19 @@ let teamsData = [
   } else {
     res.status(400).json({ status: 'error', message: 'Неверный формат данных' });
   }
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Главная страница с результатами
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Админская страница
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
 app.listen(port, () => {
